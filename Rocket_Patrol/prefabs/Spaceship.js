df4a96920abf188e9712184f86d4e67949405d7d
Edmund Liang <eliang5@ucsc.edu>
2020-04-13 16:35:17 -0700

    almost done

---
+++ Rocket_Patrol/prefabs/Spaceship.js
@@ -0,0 +1,23 @@
+// Spaceship prefab
+class Spaceship extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y, texture, frame, pointValue) {
+        super(scene, x, y, texture, frame);
+  
+        scene.add.existing(this); // add to existing, displayList, updateList
+        // store pointValue
+        this.points = pointValue;
+    }
+
+    update() {
+        // move spaceship left
+        this.x -= 3;
+        // wraparound from left to right edge
+        if(this.x <= 0 - this.width) {
+            this.x = game.config.width;
+        }
+    }
+
+    reset() {
+        this.x = game.config.width;
+    }
+}
