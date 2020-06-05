af372446165e2237972d3f5df7bf69b0c45dee07
Edmund Liang <eliang5@ucsc.edu>
2020-06-05 16:27:05 -0700

    centered credits better

--- Final_Project/src/scenes/Credits.js
+++ Final_Project/src/scenes/Credits.js
@@ -23,12 +23,12 @@ class Credits extends Phaser.Scene {
         let centerX = game.config.width / 2;
         let centerY = game.config.height / 2;
 
-        this.add.text(centerX - 200, centerY - 120, 'Programmed by Edmund Liang and Herald Hamor', menuConfig);
+        this.add.text(centerX - 240, centerY - 120, 'Programmed by Edmund Liang and Herald Hamor', menuConfig);
         this.add.text(centerX - 125, centerY - 90, 'Art by Herald Hamor', menuConfig);
-        this.add.text(centerX - 200, centerY - 30, 'Video from from Lowell Productions Stock Footage', menuConfig);
-        this.add.text(centerX - 180, centerY, 'https://www.youtube.com/watch?v=TLUERuTN3AU', menuConfig);
+        this.add.text(centerX - 250, centerY - 30, 'Video from from Lowell Productions Stock Footage', menuConfig);
+        this.add.text(centerX - 220, centerY, 'https://www.youtube.com/watch?v=TLUERuTN3AU', menuConfig);
         this.add.text(centerX - 155, centerY + 60, 'Water drop sound by gkillhour', menuConfig);
-        this.add.text(centerX - 200, centerY + 90, 'https://freesound.org/people/gkillhour/sounds/267221/', menuConfig);
+        this.add.text(centerX - 300, centerY + 90, 'https://freesound.org/people/gkillhour/sounds/267221/', menuConfig);
         this.add.text(centerX - 155, centerY + 150, 'Press F to play again', menuConfig);
         
 
